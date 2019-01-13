93326fa05350e0b4d3d927386954b0293e6ef114
k-udupa2000 <karthik.r.udupa@gmail.com>
2019-01-13 09:18:43 +0530

    Added levels and put some design

---
+++ moving.js
@@ -0,0 +1,70 @@
+function moveEverything()
+{
+    // When the game end scene is showed, there is no requirement from this function.
+    if(showingwiningscreen)
+    {
+        return;
+    }
+    computerMovement(); // This movement moves the paddle.
+    ballx = ballx + ballspeedx;
+    bally = bally + ballspeedy;
+    if(ballx < 0)
+    {
+        // Condition for paddle touching ball.
+        if(bally > paddle1y && bally < (paddle1y + PADDLE_HEIGHT))
+        {
+
+            ballspeedx = -ballspeedx;
+            deltaY = bally - (paddle1y + PADDLE_HEIGHT/2);
+            ballspeedy = deltaY*0.35;
+            playAudio();
+        }
+        // Point lost. Resetting ball.
+        else
+        {
+            computer_score += 1;
+            ballReset();
+        }
+    }
+    else if(bally < 0 || bally > canvas.height)
+    {
+        ballspeedy = -ballspeedy;
+    }
+    else if(ballx > canvas.width)
+    {
+        if(bally > paddle2y && bally < (paddle2y + PADDLE_HEIGHT)) // Indicates that paddle has blocked the ball.
+        {
+            ballspeedx = -ballspeedx;
+            deltaY = bally - (paddle2y + PADDLE_HEIGHT/2);
+            ballspeedy = deltaY*0.35;
+            playAudio();
+        }
+        // Point lost. Resetting ball.
+        else
+        {
+            player1_score += 1;
+            ballReset();
+        }
+    }
+    
+}
+
+function computerMovement()
+{
+    var move_paddle = localStorage.getItem("var_move")
+    paddle2ycenter = paddle2y + PADDLE_HEIGHT/2; // Calculating the center y coordinate of paddle 2.
+    if(paddle2ycenter < bally - 35) // If paddle 2 is above the ball then,
+    {
+        console.log('TYpe is', typeof(move_paddle))
+        console.log('Inside the movement',(move_paddle))
+        console.log('value of paddle2y', paddle2y)
+        paddle2y = paddle2y + Number(move_paddle);
+        console.log('The value after addition is ', paddle2y)
+    } 
+    else if(paddle2ycenter > bally + 35)
+    {
+      // console.log(move_paddle)
+
+        paddle2y = paddle2y - 12;
+    }
+}
